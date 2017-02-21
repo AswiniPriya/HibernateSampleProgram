52f93a66cf7e0c01868a934eefbb6985ae6bb983
ASWINI PRIYA <ASWINI PRIYA@LAPTOP-G6ACUCMH>
2017-02-21 07:40:40 +0530

    Worked with update and delete

--- HibernateProgram/src/main/java/com/aswini/StoreData.java
+++ HibernateProgram/src/main/java/com/aswini/StoreData.java
@@ -20,13 +20,20 @@ public static void main(String [] args){
     employee.setFirstName("Pari");  
     employee.setLastName("bb");  
     employee.setMobile(986524141);
+      Employee e1=new Employee();
+      e1.setId(6);
+      Employee e2=(Employee) session.load(Employee.class,new Integer(2));
+      e2.setMobile(544563593);
       
-    session.persist(employee); 
-      
+   // session.persist(employee); 
+      //session.delete(e1);
+      session.update(e2);
     t.commit();
     session.close();  
-      
-    System.out.println("successfully saved");  
-      
+   // System.out.println("successfully Deleted");  
+    //System.out.println("successfully saved");  
+    System.out.println("successfully Updated");  
+
 }	
 }
+
